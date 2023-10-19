cc7244dfd5a2064de9815753a794dc0db3bbc1d6
iamflorenz08 <dejesusflorenz8@gmail.com>
2023-10-19 16:29:30 +0800

    2023.10.19.0429

---
+++ src/utils/Lists.ts
@@ -0,0 +1,25 @@
+const months: string[] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+const genders: string[] = [
+    "Male",
+    "Female",
+    "Others",
+]
+
+export {
+    months,
+    genders,
+}
